d4efaf13938e76285bfdc4d53b96e00ad208b1bf
Brahim Amghough <bamghoug@e2r6p2.1337.ma>
2021-01-01 19:24:51 +0100

    version 0.3

--- srcs/influxdb/create_databases.sh
+++ srcs/influxdb/create_databases.sh
@@ -1,6 +1,6 @@
 influx -execute "create database nginx;
 create database phpmyadmin;
-create database garafana;
+create database grafana;
 create database wordpress;
 create database mysql;
 create database influxdb;"
